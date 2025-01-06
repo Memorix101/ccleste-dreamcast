0ccd68c0feb548e50053a6026b06aaff52263575
Memorix101 <Memorix101@users.noreply.github.com>
2025-01-06 04:53:23 +0100

    fixed endless loading when vmu is not connected

--- make_cdi.sh
+++ make_cdi.sh
@@ -1,2 +1,3 @@
  ./tools/mkdcdisc -e celeste.elf -d data -o celeste-dc.cdi -N
+ #./tools/mkdcdisc -e celeste.elf -d data -o celeste-dc_maxpadding.cdi.cdi -N
 ~/flycast-x86_64.AppImage celeste-dc.cdi
